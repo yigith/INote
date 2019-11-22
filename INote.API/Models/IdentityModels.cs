3f7b4aa92e3a2f4b3864452c1a3e84a42620fc2a
Yigit Haciefendioglu <yigith1@gmail.com>
2019-11-22 21:38:46 +0300

    migrations

--- INote.API/Models/IdentityModels.cs
+++ INote.API/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace INote.API.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("INoteContext", throwIfV1Schema: false)
         {
         }
         
