99f5a9e28b457d29a64dc92a30d1f0417a4ab033
Yigit Haciefendioglu <yigith1@gmail.com>
2019-11-25 16:51:25 +0300

    Note

---
+++ INote.API/Models/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace INote.API.Models
+{
+    [Table("Notes")]
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime? CreatedTime { get; set; }
+
+        public DateTime? ModifiedTime { get; set; }
+
+        public ApplicationUser Author { get; set; }
+    }
+}
